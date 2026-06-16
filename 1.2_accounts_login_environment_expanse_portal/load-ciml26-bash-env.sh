410cb5547941eedb49e032c7f259581bd98140db
Marty Kandes <mkandes@sdsc.edu>
2026-06-16 07:12:27 -0700

    Update load-ciml26-bash-env.sh

--- 1.2_accounts_login_environment_expanse_portal/load-ciml26-bash-env.sh
+++ 1.2_accounts_login_environment_expanse_portal/load-ciml26-bash-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Load .bashrc.ciml26 and .bash_profile.ciml26 environment.
+# Load CIML26 .bashrc and .bash_profile into your environment.
 
 cp .bashrc "${HOME}"
 cp .bash_profile "${HOME}"
@@ -10,7 +10,8 @@ cp .bash_profile.md5 "${HOME}"
 
 cd "${HOME}"
 
+chmod u-w .bashrc.md5
+chmod u-w .bash_profile.md5
+
 md5sum -c .bashrc.md5
 md5sum -c .bash_profile.md5
-
-source .bash_profile
