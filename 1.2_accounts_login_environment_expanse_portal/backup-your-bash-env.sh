7af1914a964f183b69090b4bd1064e22fcdd1511
Marty Kandes <mkandes@sdsc.edu>
2026-06-16 07:11:35 -0700

    Update backup-your-bash-env.sh

--- 1.2_accounts_login_environment_expanse_portal/backup-your-bash-env.sh
+++ 1.2_accounts_login_environment_expanse_portal/backup-your-bash-env.sh
@@ -9,3 +9,9 @@ cp -p .bash_profile .bash_profile.bak
 
 chmod u-w .bashrc.bak
 chmod u-w .bash_profile.bak
+
+md5sum .bashrc.bak > .bashrc.bak.md5
+md5sum .bash_profile.bak > .bash_profile.bak.md5
+
+chmod u-w .bashrc.bak.md5
+chmod u-w .bash_profile.bak.md5
