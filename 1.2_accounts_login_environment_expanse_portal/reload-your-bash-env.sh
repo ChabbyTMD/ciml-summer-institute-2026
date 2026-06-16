9d957135d61c7da4c5678e3011c309ccf4595922
Marty Kandes <mkandes@sdsc.edu>
2026-06-16 07:42:25 -0700

    Update reload-your-bash-env.sh

--- 1.2_accounts_login_environment_expanse_portal/reload-your-bash-env.sh
+++ 1.2_accounts_login_environment_expanse_portal/reload-your-bash-env.sh
@@ -9,3 +9,9 @@ cp .bash_profile.bak .bash_profile
 
 chmod u+w .bashrc
 chmod u+w .bash_profile
+
+cp -f .bashrc.bak.md5 .bashrc.md5
+cp -f .bash_profile.bak.md5 .bash_profile.md5
+
+md5sum -c .bashrc.md5
+md5sum -c .bash_profile.md5
