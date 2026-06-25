af2d99ce96422a52b74f51189ce5d95508f5c82e
p4rodriguez <p4rodriguez@ucsd.edu>
2026-06-25 14:41:35 -0700

    Add files via upload
    tools ecosystem issues

---
+++ 4.4_LLM_overview/get_gpuhf.sh
@@ -0,0 +1,2 @@
+/cm/shared/apps/sdsc/galyleo/galyleo launch --account sds280 --reservation ciml26gpu --qos nairr-gpu-shared-eot --partition nairr-gpu-shared --cpus 10 --memory 92 --gpus 1 --time-limit 00:30:00 --conda-yml hface3.yaml --cache 
+
